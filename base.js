db390657b810851f4f9a1971f6a0ec7ad17c4400
Fernando Nerio Jr <fernandonerio90@gmail.com>
2024-10-08 10:42:18 -0600

    feat: first commit add base.js

---
+++ base.js
@@ -0,0 +1,31 @@
+/*
+////////////////////////////////////
+  GET JOKE API
+////////////////////////////////////
+*/
+
+function getJoke(){
+  // fetch a joke from the API URL
+  fetch("https://v2.jokeapi.dev/joke/Any")
+  // it then converts the response to JSON 
+  .then(response => response.json())
+  .then(data => {
+    const setupElement = document.getElementById('setup');
+    const deliveryElement = document.getElementById('delivery');
+    
+    if(data.type === "single"){
+      setupElement.innerHTML = data.joke
+      deliveryElement.innerHTML = "";
+    }else{
+      setupElement.innerHTML = data.setup;
+      deliveryElement.innerHTML = data.delivery;
+    }
+  })
+  .catch(error => console.error("Error fetching joke", error));
+}
+
+const anotherJoke = document.getElementById('another-joke');
+
+anotherJoke.addEventListener('click', getJoke);
+
+getJoke();
